46d9ab8379d8f0873b221eabfe56af29e6dc9f3e
zzw <bitzzw@gmail.com>
2021-03-08 20:51:03 +0800

    Update rank_cy.pyx
    Add evaluation metric (cy) of vehicleid dataset

--- openunreid/core/metrics/rank_cylib/rank_cy.pyx
+++ openunreid/core/metrics/rank_cylib/rank_cy.pyx
@@ -19,7 +19,7 @@ Credit to https://github.com/luzai
 
 
 # Main interface
-cpdef evaluate_cy(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03=False):
+cpdef evaluate_cy(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03=False, use_metric_vehicleid=True):
     distmat = np.asarray(distmat, dtype=np.float32)
     q_pids = np.asarray(q_pids, dtype=np.int64)
     g_pids = np.asarray(g_pids, dtype=np.int64)
@@ -27,7 +27,10 @@ cpdef evaluate_cy(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_met
     g_camids = np.asarray(g_camids, dtype=np.int64)
     if use_metric_cuhk03:
         return eval_cuhk03_cy(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
-    return eval_market1501_cy(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
+    elif use_metric_vehicleid:
+        return eval_vehicleid_cy(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
+    else:
+        return eval_market1501_cy(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
 
 
 cpdef eval_cuhk03_cy(float[:,:] distmat, long[:] q_pids, long[:]g_pids,
@@ -231,6 +234,82 @@ cpdef eval_market1501_cy(float[:,:] distmat, long[:] q_pids, long[:]g_pids,
             avg_cmc[rank_idx] += all_cmc[q_idx, rank_idx]
         avg_cmc[rank_idx] /= num_valid_q
     
+    cdef float mAP = 0
+    for q_idx in range(num_q):
+        mAP += all_AP[q_idx]
+    mAP /= num_valid_q
+
+    return np.asarray(avg_cmc).astype(np.float32), mAP
+
+
+cpdef eval_vehicleid_cy(float[:,:] distmat, long[:] q_pids, long[:]g_pids,
+                     long[:]q_camids, long[:]g_camids, long max_rank):
+
+    cdef long num_q = distmat.shape[0]
+    cdef long num_g = distmat.shape[1]
+
+    if num_g < max_rank:
+        max_rank = num_g
+        print('Note: number of gallery samples is quite small, got {}'.format(num_g))
+
+    cdef:
+        long[:,:] indices = np.argsort(distmat, axis=1)
+        long[:,:] matches = (np.asarray(g_pids)[np.asarray(indices)] == np.asarray(q_pids)[:, np.newaxis]).astype(np.int64)
+
+        float[:,:] all_cmc = np.zeros((num_q, max_rank), dtype=np.float32)
+        float[:] all_AP = np.zeros(num_q, dtype=np.float32)
+        float num_valid_q = 0. # number of valid query
+
+        long q_idx, q_pid, g_idx
+        long[:] order = np.zeros(num_g, dtype=np.int64)
+        # long keep
+
+        float[:] raw_cmc = np.zeros(num_g, dtype=np.float32) # binary vector, positions with value 1 are correct matches
+        float[:] cmc = np.zeros(num_g, dtype=np.float32)
+        long num_g_real, rank_idx
+
+        float num_rel
+        float[:] tmp_cmc = np.zeros(num_g, dtype=np.float32)
+        float tmp_cmc_sum
+
+    for q_idx in range(num_q):
+        # get query pid
+        q_pid = q_pids[q_idx]
+
+        num_g_real = 0
+        for g_idx in range(num_g):
+            raw_cmc[num_g_real] = matches[q_idx][g_idx]
+            num_g_real += 1
+
+        # compute cmc
+        function_cumsum(raw_cmc, cmc, num_g_real)
+        for g_idx in range(num_g_real):
+            if cmc[g_idx] > 1:
+                cmc[g_idx] = 1
+
+        for rank_idx in range(max_rank):
+            all_cmc[q_idx, rank_idx] = cmc[rank_idx]
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        function_cumsum(raw_cmc, tmp_cmc, num_g_real)
+        num_rel = 0
+        tmp_cmc_sum = 0
+        for g_idx in range(num_g_real):
+            tmp_cmc_sum += (tmp_cmc[g_idx] / (g_idx + 1.)) * raw_cmc[g_idx]
+            num_rel += raw_cmc[g_idx]
+        all_AP[q_idx] = tmp_cmc_sum / num_rel
+
+    assert num_valid_q > 0, 'Error: all query identities do not appear in gallery'
+
+    # compute averaged cmc
+    cdef float[:] avg_cmc = np.zeros(max_rank, dtype=np.float32)
+    for rank_idx in range(max_rank):
+        for q_idx in range(num_q):
+            avg_cmc[rank_idx] += all_cmc[q_idx, rank_idx]
+        avg_cmc[rank_idx] /= num_valid_q
+
     cdef float mAP = 0
     for q_idx in range(num_q):
         mAP += all_AP[q_idx]
