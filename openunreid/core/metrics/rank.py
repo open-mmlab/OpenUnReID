a9575a9852762d52d56017492b3fcdc0185789e9
zzw <bitzzw@gmail.com>
2021-03-08 20:43:31 +0800

    Update rank.py
    Add evaluation metric (py) of vehicleid dataset

--- openunreid/core/metrics/rank.py
+++ openunreid/core/metrics/rank.py
@@ -152,11 +152,65 @@ def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
     return all_cmc, mAP
 
 
+def eval_vehicleid(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
+    """Evaluation with vehicleid metric
+        Key: gallery contains one images for each test vehicles and the other images in test
+             use as query
+        """
+    num_q, num_g = distmat.shape
+
+    if num_g < max_rank:
+        max_rank = num_g
+        print('Note: number of gallery samples is quite small, got {}'.format(num_g))
+
+    indices = np.argsort(distmat, axis=1)   # [5693, 800]
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+    # compute cmc curve for each query
+    all_cmc = []
+    all_AP = []
+    num_valid_q = 0.  # number of valid query
+
+    for q_idx in range(num_q):
+        remove = False     # without camid imformation remove no images in gallery
+        keep = np.invert(remove)
+        # compute cmc curve
+        raw_cmc = matches[q_idx][keep]  # binary vector, positions with value 1 are correct matches
+        if not np.any(raw_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
+
+        cmc = raw_cmc.cumsum()
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        num_rel = raw_cmc.sum()
+        tmp_cmc = raw_cmc.cumsum()
+        tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
+        tmp_cmc = np.asarray(tmp_cmc) * raw_cmc
+        AP = tmp_cmc.sum() / num_rel
+        all_AP.append(AP)
+
+    assert num_valid_q > 0, 'Error: all query identities do not appear in gallery'
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mAP = np.mean(all_AP)
+
+    return all_cmc, mAP
+
+
 def evaluate_py(
-    distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03
+    distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03, use_metric_vehicleid
 ):
     if use_metric_cuhk03:
         return eval_cuhk03(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
+    elif use_metric_vehicleid:
+        return eval_vehicleid(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
     else:
         return eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
 
@@ -169,7 +223,8 @@ def evaluate_rank(
     g_camids,
     max_rank=50,
     use_metric_cuhk03=False,
-    use_cython=True,
+    use_metric_vehicleid=True,
+    use_cython=False,
     cmc_topk=(1, 5, 10),
     verbose=True,
 ):
@@ -194,11 +249,11 @@ def evaluate_rank(
     """
     if use_cython and IS_CYTHON_AVAI:
         cmc, map = evaluate_cy(
-            distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03
+            distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03, use_metric_vehicleid
         )
     else:
         cmc, map = evaluate_py(
-            distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03
+            distmat, q_pids, g_pids, q_camids, g_camids, max_rank, use_metric_cuhk03, use_metric_vehicleid
         )
 
     if verbose:
